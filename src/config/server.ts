f5c57031aacf19a3d67aa3fcca3cc094539c0acc
DAY280130 <dimas.280130@gmail.com>
2023-09-20 21:30:53 +0700

    removed unnecessary cors

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,9 +3,4 @@ import Hapi from '@hapi/hapi';
 export const server = Hapi.server({
   port: process.env.PORT,
   host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
-  routes: {
-    cors: {
-      origin: ['http://notesapp-v1.dicodingacademy.com'],
-    },
-  },
 });
